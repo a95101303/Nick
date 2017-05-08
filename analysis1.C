bed00d855dcd45bcd0c479630d4b8c31129d00c1
Nick <a9510130375@gmail.com>
2017-05-08 20:03:35 +0800

    add

--- analysis1.C
+++ analysis1.C
@@ -11,22 +11,32 @@ void analysis1()
 
 
 	//create the histogram that we want to see
-	float gauspeak[36];
-	t1->SetBranchAddress("gauspeak",&gauspeak[36]);
-	
+	float *gauspeak;
+	t1->SetBranchAddress("gauspeak",&gauspeak);
+	cout << gauspeak[0] << endl;
+	cout << gauspeak[1] << endl;
+	cout << gauspeak[16] << endl;
 	//h_photek_time,h_int[17]-> TH1F histogram
 	TH1F *h_photek_time = new TH1F("h_tphotek_time","photek time",1000,-100,100);
 	h_photek_time->SetXTitle("x");
 	h_photek_time->SetYTitle("time");
 	TCanvas* c1= new TCanvas("c1");
      		
-	//Long64_t nentries =t1 ->GetEntries();
-	//for(Long64_t i=0;i<nentries;i++) {
-	//	t1->GetEntry(i);
+	Long64_t nentries =t1 ->GetEntries();
+	
+	for(Long64_t i=0;i<nentries;i++) {
+		//t1->GetEntry(i);
+		cout << gauspeak[16] << endl;
 		h_photek_time->Fill(gauspeak[16]);
      		h_photek_time->SetLineColor(2);
 	
-
+	}
 	//set in pdf and print
+	h_photek_time->Draw();
 	c1->Print("photek_time_plot.pdf");
+	
+
+	TFile* outFile = new TFile("analysis1.root","recreate");
+	h_photek_time->Write();
+	outFile->Close();
 }
